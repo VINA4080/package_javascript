9da556b36a99f63ae1aba8c15df1b2d4809c5c28
VINA4080 <vinaandrianarisoa1@gmail.com>
2022-09-09 13:56:14 +0300

    new commit

--- assets/script.js
+++ assets/script.js
@@ -6,16 +6,22 @@ let bg2 = document.getElementById("bg2");
 let bg3 = document.getElementById("bg3");
 
 btn1.addEventListener("click", () => {
-    bg1.classList.remove("bg-primary");
-    bg1.classList.add("bg-warning");
+    bg1.classList.toggle("bg-warning");
 })
 
 btn2.addEventListener("click", () => {
-    bg2.classList.remove("bg-danger");
-    bg2.classList.add("bg-primary");
+    bg2.classList.toggle("bg-dark");
 })
 
 btn3.addEventListener("click", () => {
-    bg3.classList.remove("bg-warning");
-    bg3.classList.add("bg-success");
+    bg3.classList.toggle("bg-danger");
 })
+
+// const buttons = document.querySelectorAll(".btn");
+// const bg = document.querySelectorAll(".box");
+
+// for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("click", () => {
+//         bg[i].classList.toggle('red');
+//     })
+// }
